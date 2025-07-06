850b2185ef286a3b26da465745d336028779c712
George Ogden <george-ogden@outlook.com>
2025-07-06 18:28:18 +0100

    Add pragma once tests

---
+++ tests/pragma-once/test_no_pragma.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+DIRECTORY=$(dirname $0)
+LOG=`mktemp`
+
+pre-commit try-repo . pragma-once -v --files $DIRECTORY/test_data/no_pragma.h++ | tee $LOG && exit 1
+grep -F no_pragma.h++ $LOG
+grep '^No' $LOG
