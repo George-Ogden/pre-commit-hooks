676dc3588bdb16a1454b31aa91e14e2f4a84eb67
George Ogden <george-ogden@outlook.com>
2025-07-18 16:54:53 +0100

    Test merge conflict hook

---
+++ tests/merge-conflict/test_rst_conflict.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+DIRECTORY=$(dirname $0)
+LOG=`mktemp`
+
+pre-commit try-repo . check-merge-conflict -v --files $DIRECTORY/test_data/conflict.rst | tee $LOG && exit 1
+grep -F conflict.rst:1 $LOG | grep '<<<<<<<'
+grep -F conflict.rst:7 $LOG | grep '>>>>>>>'
