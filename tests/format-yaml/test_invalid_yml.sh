da7ec7f78ee48e64e653c162433eb1f140eb4ca2
George Ogden <george-ogden@outlook.com>
2025-07-18 10:31:53 +0100

    Remove format YAML hook

--- tests/format-yaml/test_invalid_yml.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-DIRECTORY=$(dirname $0)
-
-pre-commit try-repo . format-yaml -v --files $DIRECTORY/test_data/invalid.yml && exit 1
-diff $DIRECTORY/test_data/invalid{-expected.yaml,.yml}
