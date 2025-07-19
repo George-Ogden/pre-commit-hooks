31feab7f5835fe9f8181a1177dd5aab9d0478277
George Ogden <george-ogden@outlook.com>
2025-07-19 11:31:12 +0100

    Escape characters in printf

--- validate-pragma.sh
+++ validate-pragma.sh
@@ -9,9 +9,9 @@ EXITCODE=0
 for file in "$@"; do
     sed "/$INCLUDE_PATTERN/q" "$file" | grep -qE "$PRAGMA_PATTERN" || {
         if grep -qE "$PRAGMA_PATTERN" "$file"; then
-            printf "\`#pragma once\` found in $file but after the first \`#include\`\n"
+            printf "\`#pragma once\` found in %s but after the first \`#include\`\n" "$file"
         else
-            printf "No \`#pragma once\` found in $file\n"
+            printf "No \`#pragma once\` found in %s\n" "$file"
         fi
         EXITCODE=1
     }
