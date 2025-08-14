8dbe1972ab76824e348dda2c44094d8b774c39ac
George Ogden <george-ogden@outlook.com>
2025-08-14 11:38:05 +0200

    Check for Python debug imports

--- validate-dbg.sh
+++ validate-dbg.sh
@@ -16,6 +16,12 @@ for file in "$@"; do
         EXITCODE=1
     done < "$LOG";
 
+    grep -nE '^(.*[[:space:]])?import[[:space:]](.*[[:space:]])?dbg([[:space:]].*)?$' "$file" > "$LOG"
+    while IFS= read -r line; do
+        printf "\`dbg.h\` import found in %s:%s\n" "$file" "$line"
+        EXITCODE=1
+    done < "$LOG";
+
 done
 
 exit $EXITCODE
