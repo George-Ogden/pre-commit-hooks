e8a329fb1b0a2caca04d20af9a11bd92f2d7f61d
George Ogden <george-ogden@outlook.com>
2025-08-31 16:29:27 +0200

    Ignore hidden files with todo check

--- tests/todo-check/test_included_in_pre_commit.sh
+++ tests/todo-check/test_included_in_pre_commit.sh
@@ -6,8 +6,8 @@ DIRECTORY=$(dirname $0)
 LOG=`mktemp`
 ROOT=`pwd`
 
-cd $DIRECTORY/test_data/mypy_1_16_1
+cd $DIRECTORY/test_data/pre-commit-config
 git init
 pre-commit install
-pre-commit run mypy --files *
+pre-commit run todo-check --files *  .*
 rm -rf .git
