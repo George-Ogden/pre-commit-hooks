8e87657544a4a353fdae3098b8726e77b161f512
George Ogden <george-ogden@outlook.com>
2025-10-05 22:16:35 +0200

    Pass flags to codespell

--- run-codespell.sh
+++ run-codespell.sh
@@ -2,8 +2,7 @@
 
 set -o pipefail
 
-FILENAME="$1"
+FILENAME="${@: -1}"
 ESCAPED_FILENAME=$(printf '%s\n' "$FILENAME" | sed -e 's/[]\/$*.^[]/\\&/g')
-
 
 codespell $@ | sed -E "s,$ESCAPED_FILENAME:[[:digit:]]+,commit message error,"
---
+++ tests/spell-check/test_ignore_error.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+DIRECTORY=$(dirname $0)
+
+pre-commit run --config $DIRECTORY/test_data/ignore-error/.pre-commit-config.yaml --hook-stage commit-msg --commit-msg-filename $DIRECTORY/test_data/ignore-error/msg.txt
