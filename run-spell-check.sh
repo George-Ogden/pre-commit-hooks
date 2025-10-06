e0258ca481729d0a1f4b401f2776e2b925fc701f
George Ogden <george-ogden@outlook.com>
2025-10-06 08:53:24 +0200

    Change spell change language to script

--- run-spell-check.sh
+++ run-spell-check.sh
@@ -5,4 +5,4 @@ set -o pipefail
 FILENAME="${@: -1}"
 ESCAPED_FILENAME=$(printf '%s\n' "$FILENAME" | sed -e 's/[]\/$*.^[]/\\&/g')
 
-codespell $@ | sed -E "s,$ESCAPED_FILENAME:[[:digit:]]+,commit message error,"
+pip install uv && uv pip install codespell && codespell $@ | sed -E "s,$ESCAPED_FILENAME:[[:digit:]]+,commit message error,"
