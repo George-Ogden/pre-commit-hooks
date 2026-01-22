46ce67aa66157745ce5636a6a999cb4644dd7dcb
George Ogden <george-ogden@outlook.com>
2026-01-22 21:29:32 +0000

    Add failing test cases for when mypy fails to install requirements

---
+++ tests/mypy/test_invalid_requirements_success.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+DIRECTORY=$(dirname $0)
+
+pre-commit run --config $DIRECTORY/test_data/invalid-requirements-file/.pre-commit-config.yaml --files $DIRECTORY/test_data/invalid-requirements-file/good.py -v
