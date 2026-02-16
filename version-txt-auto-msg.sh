d64081ddde0da633123e48f3a64d41a0f090b6cb
George Ogden <george-ogden@outlook.com>
2026-02-16 13:57:09 +0000

    Only add message when empty

--- version-txt-auto-msg.sh
+++ version-txt-auto-msg.sh
@@ -9,7 +9,7 @@ COMMIT_MSG_FILE=$1
 OLD_VERSION=$(git show HEAD:$FILE 2>/dev/null || true)
 NEW_VERSION=$(cat $FILE || true)
 
-if [ ! -s "$COMMIT_MSG_FILE" ] && [ -n "$OLD_VERSION" ] && [ "$OLD_VERSION" != "$NEW_VERSION" ]; then
+if [ ! -s "$COMMIT_MSG_FILE" ] && [ -n "$OLD_VERSION" ] && [ -n "$OLD_VERSION" ] && [ "$OLD_VERSION" != "$NEW_VERSION" ]; then
     MSG="Bump version $OLD_VERSION -> $NEW_VERSION"
-    echo $MSG > "$1"
+    echo "$MSG" > "$COMMIT_MSG_FILE"
 fi
