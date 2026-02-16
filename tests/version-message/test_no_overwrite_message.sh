98d4d04d2067bf4b62c5af765eac5f48f38c3127
George Ogden <george-ogden@outlook.com>
2026-02-16 14:30:32 +0000

    Setup bot accounts for test commits

--- tests/version-message/test_no_overwrite_message.sh
+++ tests/version-message/test_no_overwrite_message.sh
@@ -6,6 +6,8 @@ CWD=`pwd`
 
 cd "$(mktemp -d)"
 git init
+git config user.email github-actions[bot]@users.noreply.github.com
+git config user.name "github-actions[bot]"
 cp $DIRECTORY/test_data/{.pre-commit-config.yaml,version.txt} .
 sed -i "s,repo: \.,repo: $CWD," .pre-commit-config.yaml
 
