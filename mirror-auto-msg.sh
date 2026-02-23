f83c48456633b0742e5e61239e4a78fefc6cb206
George Ogden <george-ogden@outlook.com>
2026-02-23 21:46:25 +0000

    Write mirror auto msg hook

---
+++ mirror-auto-msg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -e
+
+FILE=".mirror.lock"
+COMMIT_MSG_FILE=$1
+
+if [ ! -s "$COMMIT_MSG_FILE" ] && [ -f "$FILE" ] && git diff --quiet HEAD -- "$FILE"; then
+    if git cat-file -e "HEAD:$FILE" 2>/dev/null; then
+        MSG="Sync mirror"
+    else
+        MSG="Install mirror"
+    fi
+    echo "$MSG" > "$COMMIT_MSG_FILE"
+fi
