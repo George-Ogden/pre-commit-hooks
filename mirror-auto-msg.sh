5d03ab3d11d46db8f2e34c03fda4f4a134c42e21
George Ogden <george-ogden@outlook.com>
2026-02-23 21:47:09 +0000

    Sync mirror

--- mirror-auto-msg.sh
+++ mirror-auto-msg.sh
@@ -6,7 +6,7 @@ set -e
 FILE=".mirror.lock"
 COMMIT_MSG_FILE=$1
 
-if [ ! -s "$COMMIT_MSG_FILE" ] && [ -f "$FILE" ] && git diff --quiet HEAD -- "$FILE"; then
+if [ ! -s "$COMMIT_MSG_FILE" ] && [ -f "$FILE" ] && ! git diff --quiet HEAD -- "$FILE"; then
     if git cat-file -e "HEAD:$FILE" 2>/dev/null; then
         MSG="Sync mirror"
     else
